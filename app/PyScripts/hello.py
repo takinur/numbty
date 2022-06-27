8d415672d2e8a91f67e2811577e075345afe8f78
TAKINUR <takinurm@gmail.com>
2022-06-28 00:10:23 +0600

    Added python Requirements

--- app/PyScripts/hello.py
+++ app/PyScripts/hello.py
@@ -1,9 +1,5 @@
 import json
-# from rich import print, print_json
-#Find the even numbers in a list
-# for i in range(1,11):
-#     if i%2==0:
-#         print(i)
+
 x = {
   "name": "John",
   "age": 30,
