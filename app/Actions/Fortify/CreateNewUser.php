9e63e3931846eab2b8737f1b2710abd6a042fe84
TAKINUR <takinurm@gmail.com>
2022-08-10 00:04:57 +0600

    improvements for permisions

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -24,13 +24,22 @@ class CreateNewUser implements CreatesNewUsers
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
+            //role' => 'required|in:customer,driver', //Accept only 2 Role
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        //TODO:ADD Roles to newly created user
+
+        // $user->assignRole($input['role']);
+        //TODO: Send mail notification to admin
+
+        return $user;
+
     }
 }
